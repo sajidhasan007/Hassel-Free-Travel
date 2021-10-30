d7387b9536492f58a73126cd32725a561b9914d9
sajidhasan007 <sajidhasan1997@gmail.com>
2021-10-31 01:10:24 +0600

    add style in home page

--- src/Pages/Allevents/Allevents.js
+++ src/Pages/Allevents/Allevents.js
@@ -9,8 +9,9 @@ const Allevents = (props) => {
     const { _id, event_name, event_img, description, event_fee, max_mamber, date } = events;
     // console.log(_id, event_name, event_img);
     return (
-        <Col key={events._id} className='card-color'>
-            <Card className='h-100 '>
+
+        <Col key={events._id} >
+            <Card className='h-100 div1'>
                 <div className='d-flex justify-content-center overflow'>
                     <Card.Img className='my-coustom d-flex justify-content-center' variant="top" src={event_img} />
                 </div>
@@ -19,12 +20,25 @@ const Allevents = (props) => {
                     <Card.Text>
                         {description.slice(0, 90)}
                         <span> ...</span>
+
                     </Card.Text>
-                    <Link className='me-3' to={`/book/${_id}`}><button className="btn btn-primary">Book Event</button></Link>
-                    <Link to={`/event/${_id}`}><button className="btn btn-warning">Details</button></Link>
+
+
+
+
                 </Card.Body>
+                <div className='d-flex align-items-end'>
+                    <div className='p-3'>
+                        <Link className='me-3' to={`/book/${_id}`}><button className="btn btn-primary">Book Event</button></Link>
+                        <Link to={`/event/${_id}`}><button className="btn btn-warning">Details</button></Link>
+                    </div>
+                </div>
             </Card>
         </Col>
+
+
+
+
     );
 };
 
